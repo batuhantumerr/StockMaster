4affe95545b1d3c099af62e27c74f10a07ce9346
batuhantumerr <tmr_bthn@hotmail.com>
2026-01-07 11:06:57 +0300

    feat: Generic Repository and UnitOfWork patterns implemented

---
+++ StockMaster.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockMaster.Core.Repositories
+{
+    internal interface IGenericRepository
+    {
+    }
+}
---
+++ StockMaster.Infrastructure/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockMaster.Infrastructure.UnitOfWorks
+{
+    internal class UnitOfWork
+    {
+    }
+}
